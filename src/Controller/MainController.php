a5a4989f61ebaad325f24b930083afd5a7828ae4
Chervera <chervera@gmail.com>
2018-10-09 19:15:12 +0200

    canonical urls, sitemap, github link and minnor fixes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,19 +5,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use App\Entity\Article;
+use App\Entity\Tag;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 
 class MainController extends BaseController {
 
     const ARTICLES_TO_SHOW_HOMEPAGE = 3;
     const PAGE_SIZE = 1;
-    
-    private $articleService;
-
-    public function __construct(\App\Service\ArticleService $articleService) {
-        $this->articleService = $articleService;
-    }
 
     /**
      * @Route("/", name="main")
@@ -45,15 +42,16 @@ class MainController extends BaseController {
     }
 
     /**
-     * @Route("/blog/tag/{tag}.html", name="articles_tag")
+     * @Route("/blog/tag/{slug}.html", name="articles_tag")
      */
-    public function articlesByTag(Request $request, $tag) {
+    public function articlesByTag(Request $request, $slug) {
 
         $page = $request->query->get('page');
         if (!$page) {
             $page = 1;
         }
-        $articlesPaginateds = $this->getDoctrine()->getRepository(Article::class)->findByTagActiveOrderByCreatedOnPaginated($tag);
+        $tag = $this->getDoctrine()->getRepository(Tag::class)->findOneBySlug($slug);
+        $articlesPaginateds = $this->getDoctrine()->getRepository(Article::class)->findByTagActiveOrderByCreatedOnPaginated($slug);
         $paginator = $this->createPaginator($articlesPaginateds, self::PAGE_SIZE, $page);
 
         return $this->render('articles.html.twig', array('articles' => $paginator, 'tag' => $tag));
@@ -68,17 +66,39 @@ class MainController extends BaseController {
 
         return $this->render('article.html.twig', array('article' => $article));
     }
-    
+
     /**
      * @Route("/sitemap.xml", name="sitemap")
      */
-    public function sitemap($slug) {
+    public function sitemap() {
+        $urls = array();
 
-        $article = $this->getDoctrine()->getRepository(Article::class)->findOneBySlug($slug);
+        $urls[] = array('loc' => $this->generateUrl('main', [], UrlGeneratorInterface::ABSOLUTE_URL));
+        $urls[] = array('loc' => $this->generateUrl('articles', [], UrlGeneratorInterface::ABSOLUTE_URL));
 
-        return $this->render('article.html.twig', array('article' => $article));
+
+        $package = new Package(new EmptyVersionStrategy());
+
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findByActive(true);
+        foreach ($articles as $article) {
+            $images = array(
+            'loc' => 'img/articles/' . $article->getImage(),
+            'title' => $article->getTitle()
+            );
+            $urls[] = array('loc' => $this->generateUrl('article', ['slug' => $article->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL),
+                            'image' => $images);
+        }
+
+        $tags = $this->getDoctrine()->getRepository(Tag::class)->findAll();
+        foreach ($tags as $tag) {
+            $urls[] = array('loc' => $this->generateUrl('articles_tag', ['slug' => $tag->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL));
+        }
+
+        // return response in XML format
+        $response = new Response($this->renderView('sitemap.html.twig', array('urls' => $urls), 200));
+        $response->headers->set('Content-Type', 'text/xml');
+
+        return $response;
     }
-    
-    
 
 }
